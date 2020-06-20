5f1554ebcc1878688a81c76218102e0be988d1a8
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-06-20 14:30:29 +0100

    Configure setup

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # weightGIS
-Weight ESRI shapefiles attributes
+
+weightGIS is designed to allow you to simply create a cross relationship weight in both area and sub unit population
+(where available). There may be instances where you have a panel of shapefiles, but need to standardise them over time.
+Border changes that are not reflective of underling changes can cause empirical problems but data will exist for these
+places based on those changes. By standardising to a base year, this can be resolved, and data that is held can then 
+be weighted accordingly. 
+
+
+## Getting started
+weightGIS is available via Pypi so you can just pip install it, if your a windows user the following command can
+be used from the terminal. 
+
+```shell script
+python -m pip install weightGIS
+```
+
+A full worked example via a jupyter notebook and some example data is available within the Example folder of this 
+repository.
+
+<!-- CONTRIBUTIONS -->
+## Contributions
+Contributions are always welcome, if you want to make a contribution simply make a pull request based on your fork of
+the project or however else you feel able too. 
+
+<!-- License -->
+## License
+Distributed under the MIT License. See `LICENSE` for more information.
+
---
+++ setup.py
@@ -0,0 +1,63 @@
+# Copyright (C) 2020 Samuel Baker
+
+DESCRIPTION = "Weight ESRI shapefiles attributes"
+LONG_DESCRIPTION = """
+# weightGIS
+
+shapefileWeights purpose is to allow you to simply create a cross relationship weight in both area and sub unit 
+population (where available). All the source code is available from [the github page][repo] along with a full tutorial 
+on how to use the module via a jupyter notebook alongside some example data.
+
+[repo]: https://github.com/sbaker-dev/weightGIS
+
+"""
+LONG_DESCRIPTION_CONTENT_TYPE = "text/markdown"
+
+DISTNAME = 'weightGIS'
+MAINTAINER = 'Samuel Baker'
+MAINTAINER_EMAIL = 'samuelbaker.researcher@gmail.com'
+LICENSE = 'MIT'
+DOWNLOAD_URL = "https://github.com/sbaker-dev/weightGIS"
+VERSION = "0.01.0"
+PYTHON_REQUIRES = ">=3.6"
+
+INSTALL_REQUIRES = [
+
+    'csvObject', 'shapeObject', 'shapely']
+
+PACKAGES = [
+    "weightGIS",
+]
+
+CLASSIFIERS = [
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'License :: OSI Approved :: MIT License',
+]
+
+if __name__ == "__main__":
+
+    from setuptools import setup
+
+    import sys
+
+    if sys.version_info[:2] < (3, 7):
+        raise RuntimeError("weightGIS requires python >= 3.7.")
+
+    setup(
+        name=DISTNAME,
+        author=MAINTAINER,
+        author_email=MAINTAINER_EMAIL,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+        license=LICENSE,
+        version=VERSION,
+        download_url=DOWNLOAD_URL,
+        python_requires=PYTHON_REQUIRES,
+        install_requires=INSTALL_REQUIRES,
+        packages=PACKAGES,
+        classifiers=CLASSIFIERS
+    )
