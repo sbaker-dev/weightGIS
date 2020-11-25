641359897a8082309b663fd9f316c6d38d214b84
sbaker-dev <samuelbaker.researcher@gmail.com>
2020-11-25 11:49:22 +0000

    version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ MAINTAINER = 'Samuel Baker'
 MAINTAINER_EMAIL = 'samuelbaker.researcher@gmail.com'
 LICENSE = 'MIT'
 DOWNLOAD_URL = "https://github.com/sbaker-dev/weightGIS"
-VERSION = "0.06.0"
+VERSION = "0.06.1"
 PYTHON_REQUIRES = ">=3.6"
 
 INSTALL_REQUIRES = [
