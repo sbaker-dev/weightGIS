0a509ca846350a6a2d223fa323c282e4deeeae4c
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-01-11 14:12:24 +0000

    Restructuring package source code

--- weightGIS/__init__.py
+++ weightGIS/__init__.py
@@ -1,5 +1,7 @@
+
+from weightGIS.weighting.ConstructWeights import ConstructWeights
+from weightGIS.weighting.AssignWeights import AssignWeights
+from weightGIS.weighting.WeightExternal import WeightExternal
+
 from weightGIS.AdjacentRelations import AdjacentRelations
-from weightGIS.ConstructWeights import ConstructWeights
-from weightGIS.WeightExternal import WeightExternal
-from weightGIS.AssignWeights import AssignWeights
 from weightGIS.GeoLookup import GeoLookup
