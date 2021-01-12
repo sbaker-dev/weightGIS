4f36121d182b648de558590d9ea81e2957d9b508
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-01-12 10:44:59 +0000

    Formatting adjustments to be seperate and making paths explict

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ MAINTAINER = 'Samuel Baker'
 MAINTAINER_EMAIL = 'samuelbaker.researcher@gmail.com'
 LICENSE = 'MIT'
 DOWNLOAD_URL = "https://github.com/sbaker-dev/weightGIS"
-VERSION = "0.07.0"
+VERSION = "0.08.0"
 PYTHON_REQUIRES = ">=3.6"
 
 INSTALL_REQUIRES = [
--- weightGIS/__init__.py
+++ weightGIS/__init__.py
@@ -3,6 +3,7 @@ from weightGIS.weighting.Access.access_weighted import access_weighted
 from weightGIS.weighting.Access.SQLParser import SQLParser
 
 from weightGIS.weighting.ConstructWeights import ConstructWeights
+from weightGIS.weighting.AdjustWeights import AdjustWeights
 from weightGIS.weighting.AssignWeights import AssignWeights
 from weightGIS.weighting.WeightExternal import WeightExternal
 
