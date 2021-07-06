13e27bcc29fff972660dfc932aaa91bedde65382
sbaker-dev <samuelbaker.researcher@gmail.com>
2021-07-06 12:42:39 +0100

    Get description from readme

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
-# Copyright (C) 2020 Samuel Baker
+# Copyright (C) 2021 Samuel Baker
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
 
 DESCRIPTION = "Weight ESRI shapefiles attributes"
-LONG_DESCRIPTION = """
-# weightGIS
-
-shapefileWeights purpose is to allow you to simply create a cross relationship weight in both area and sub unit 
-population (where available). All the source code is available from [the github page][repo] along with a full tutorial 
-on how to use the module via a jupyter notebook alongside some example data.
-
-[repo]: https://github.com/sbaker-dev/weightGIS
-
-"""
 LONG_DESCRIPTION_CONTENT_TYPE = "text/markdown"
 
 DISTNAME = 'weightGIS'
@@ -47,7 +43,7 @@ if __name__ == "__main__":
         maintainer=MAINTAINER,
         maintainer_email=MAINTAINER_EMAIL,
         description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
+        long_description=readme(),
         long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
         license=LICENSE,
         version=VERSION,
