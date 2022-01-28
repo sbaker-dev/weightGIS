2eb267dc0eee597226e399bbe2d47ea0c323fc99
sbaker-dev <samuelbaker.researcher@gmail.com>
2022-01-28 15:55:19 +0000

    Renaming for consistentcy

--- weightGIS/Cleaning/__init__.py
+++ weightGIS/Cleaning/__init__.py
@@ -1,5 +1,5 @@
 from .FormatDataclasses import Match, Correction
-from .FormatSetup import Standardise
+from .FormatStandardise import Standardise
 from .FormatNames import FormatNames
 from .FormatRelational import RelationalDatabase
 from .FormatExternal import FormatExternal
