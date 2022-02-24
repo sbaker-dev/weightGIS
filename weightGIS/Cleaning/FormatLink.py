d87f235cd76a38fd37fd41744a9df4d30ed82a0d
sbaker-dev <samuelbaker.researcher@gmail.com>
2022-02-24 09:26:57 +0000

    Have write out be a little cleaner

--- weightGIS/Cleaning/FormatLink.py
+++ weightGIS/Cleaning/FormatLink.py
@@ -12,16 +12,17 @@ class FormatLink:
         self.database = {}
 
     def __call__(self, data_dir, write_dir, database_name):
-        [self._run(CsvObject(Path(data_dir, file))) for file in directory_iterator(data_dir)]
+        [self._run(CsvObject(Path(data_dir, file)), i) for i, file in enumerate(directory_iterator(data_dir))]
         print(f"Linked Data {terminal_time()}")
 
         write_json(self.database, write_dir, f'Cleaned_{database_name}')
         print(f"Written Data {terminal_time()}")
 
-    def _run(self, csv_file):
+    def _run(self, csv_file, index):
         """Link the data to the database"""
         file_name = csv_file.file_name
-        print(file_name)
+        if index % 100 == 0:
+            print(f"Linked {index} files up to {file_name}")
 
         self.database[file_name] = {self.matcher[row[0]]: {h: row[i + 1] for i, h in enumerate(csv_file.headers[1:])}
                                     for row in csv_file.row_data}
